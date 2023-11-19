e4c1077022f29c2227d0defe1da9c4fd8c7ef2da
HLH <305701970@qq.com>
2023-11-19 16:48:11 +0800

    新增组件head

---
+++ src/components/WebHead.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="web-contaienr">
+    {{ title }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  // 是否显示
+  title: {
+    type: String,
+    default: "标题",
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.web-contaienr {
+  width: 100%;
+  height: 0.45rem;
+  background: #0f5bff;
+  text-align: center;
+  line-height: 0.45rem;
+  color: #fff;
+}
+</style>
